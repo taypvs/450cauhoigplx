cf71f3cc1ce67658f190e28820a15c4ea8b02205
tay.pham <sontay.pham@gmail.com>
2017-03-03 17:11:01 +0700

    Recode

---
+++ 450CauHoiGPLX/Assets/Scripts/WheelScripts/TargetPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPoint : MonoBehaviour {
+
+	public bool isCheckPoint;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

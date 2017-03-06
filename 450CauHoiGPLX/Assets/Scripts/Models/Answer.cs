92a071bfd29eefcb8d848ea48578560fb3ba639f
vfa-taypvs <phamvietsontay@Phams-MacBook-Pro.local>
2017-03-06 23:03:39 +0700

    - Add Boomlagoon Json Lib

---
+++ 450CauHoiGPLX/Assets/Scripts/Models/Answer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Boomlagoon.JSON;
+
+public class Answer : MonoBehaviour {
+
+	public string id;
+	public string text;
+	public string position;
+	public string correct;
+	public string question_id;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Answer(){
+	
+	}
+
+	public Answer(JSONObject jsonObject){
+		id = jsonObject ["id"].Str;
+		text = jsonObject ["text"].Str;
+		position = jsonObject ["position"].Str;
+		correct = jsonObject ["correct"].Str;
+		question_id = jsonObject ["question_id"].Str;
+	}
+}
